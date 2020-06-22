27a895fb88f9df5c0af748bbd0f45cf17e8a64ae
overtomanu <overtomanu@outlook.com>
2020-06-22 12:13:24 +0530

    initial commit

---
+++ src/main/java/prj/sample/SampleSpringLogbackJaninoProjectApplication.java
@@ -0,0 +1,13 @@
+package prj.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringLogbackJaninoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringLogbackJaninoProjectApplication.class, args);
+	}
+
+}
